d6f6a548ef51e9dd495bc194b4fc1b135dc71e7b
Aryan Yadav <ay6033756@gmail.com>
2026-03-05 02:07:50 +0530

    fix(mobile): reduce scroll sensitivity, shrink card stack

--- src/components/SmoothScroll.tsx
+++ src/components/SmoothScroll.tsx
@@ -10,7 +10,7 @@ gsap.registerPlugin(ScrollTrigger);
 export default function SmoothScroll({ children }: { children: React.ReactNode }) {
   useEffect(() => {
     const lenis = new Lenis({
-      duration: 1.4,
+      duration: 1.2,
       easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
       orientation: "vertical",
       gestureOrientation: "vertical",
